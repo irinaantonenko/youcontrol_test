64c389c86302484aa0b2fe8b900256b7f0f65bee
Iryna Antonenko <irishkajatsenko@gmail.com>
2022-07-11 02:41:36 +0300

    added tabs

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -43,3 +43,17 @@ function myFunction() {
     header.classList.remove("header__fixed");
     }
 }
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabs__content");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tabs__links");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
