10e3d04d21ab15d6e9d17b627c79998299dd875e
Iryna Antonenko <irishkajatsenko@gmail.com>
2022-07-10 23:43:05 +0300

    added content of dropdown and fixed scrollbar

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,6 +1,7 @@
 function showMenu() {
   document.getElementById("myDropdown").classList.toggle("dropdown__show");
-  document.getElementById("close").classList.toggle("dropdown__show-close");
+  document.getElementById("close").classList.toggle("dropdown__show-close"); 
+  document.body.classList.toggle("hidden");
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -18,15 +19,27 @@ function showMenu() {
 })*/
 
 function addActive() {
-  document.getElementById("loudspeaker").classList.toggle("active");   
+  document.getElementById("loudspeaker").classList.toggle("active");  
 }
 
 function closeMenu() {
   document.getElementById("myDropdown").classList.remove("dropdown__show");
   document.getElementById("loudspeaker").classList.remove("active");
   document.getElementById("close").classList.remove("dropdown__show-close");
+  document.body.classList.toggle("hidden");  
 }
 
 function removeMessage() {
   document.getElementById("icon-block").classList.add("remove");
+}
+
+window.onscroll = function() {myFunction()};        
+let header = document.getElementById("myHeader");
+let sticky = header.offsetTop;        
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+    header.classList.add("header__fixed");
+    } else {
+    header.classList.remove("header__fixed");
+    }
 }
